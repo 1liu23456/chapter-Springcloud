f7d6e039597b704dd6fa832f4f72e839404a2679
1liu23456 <1278871262@qq.com>
2020-12-22 15:09:54 +0800

    完成springcloud书籍的Eureka集群部分

---
+++ eureka-server8761/src/main/java/com/lxy/cloud/EurekaServerApplication8761.java
@@ -0,0 +1,14 @@
+package com.lxy.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableEurekaServer
+public class EurekaServerApplication8761 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication8761.class,args);
+    }
+}
